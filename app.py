3fd27c16b7653fff59000caf57136b4158e4ffd0
woojin <woojinnj@naver.com>
2026-08-24 19:59:39 +0900

    오류 수정, requirements 2개 있던거 + 폴더 경로 오류

--- app.py
+++ app.py
@@ -4,14 +4,20 @@ app =Flask(__name__)
 
 
 @app.route('/')
-def main():
-    return render_template('')
+def index():
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__=='__main__':
     app.run(debug=True)
