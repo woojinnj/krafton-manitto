66b081d07463b6c582392ec3f56d8b37e2e9a340
woojin <woojinnj@naver.com>
2026-08-25 16:12:43 +0900

    dd

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash, check_password_hash
 import random
