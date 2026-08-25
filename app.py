326cd2e0275799a05ddf568b991c6e9f7270314a
pinn99k <minn90311@gmail.com>
2026-08-25 11:31:50 +0900

    ignore

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, jsonify, render_template
 from pymongo import MongoClient
+import random
 
 # db name : krafton_users
 # db 요소 : _id uid pwd name mbti want rating targetId
 
 app =Flask(__name__)
-# client = MongoClient("이름 + 로컬 주소입력")
-# db = client.kraftonUsers
+client = MongoClient("이름 + 로컬 주소입력")
+db = client.kraftonUsers
 
 @app.route('/')
 def index():
@@ -26,6 +27,11 @@ def dashboard():
 
 @app.route('/api/shuffle', methods=['POST'])
 def api_shuffle():
+    # 데이터베이스를 가져오기
+    all_users = list(db.KraftonUsers.find())
+
+    # 셔플하기
+    for a in 
     return jsonify({'result': 'success'})
 
 if __name__=='__main__':
