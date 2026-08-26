ded0553b535bd5cfc7c2346cb5f7c26c60b66a8b
woojin <woojinnj@naver.com>
2026-08-26 19:15:03 +0900

    port changed

--- app.py
+++ app.py
@@ -465,4 +465,4 @@ def get_game_status():
 #     return jsonify({"result": "success", "inserted": len(dummy_users)})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
